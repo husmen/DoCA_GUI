577f8a3c5c4999d765cbc306416f5221e86d95d5
Houssem MENHOUR <husmen@users.noreply.github.com>
2019-02-25 07:19:45 +0300

    typo

--- main_gui.py
+++ main_gui.py
@@ -77,7 +77,7 @@ class Window(QMainWindow):
         ''' docstring '''
 
         #self.title = 'KOU DoSA GUI | {}'.format(self.config['DEFAULT']['username'])
-        self.title = 'DoCA | Document Classification and Analysis')
+        self.title = 'DoCA | Document Classification and Analysis'
  
         self.table_widget = MyTableWidget(self, self.config)
         self.setCentralWidget(self.table_widget)
